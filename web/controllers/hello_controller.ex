5361d94d23cb198e9700371c5344dfd5ba46897b
byelipk <byelipk@gmail.com>
2016-08-25 14:47:57 -0400

    Create a function plug inside HelloController

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,12 +2,20 @@ defmodule HelloPhoenix.HelloController do
 
   use HelloPhoenix.Web, :controller
 
+  plug :put_headers, %{ content_encoding: "gzip", cache_control: "max-age=3600", hacked: "questionable" }
+
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def show(conn, %{"messenger" => messenger}) do
     render conn, "show.html", messenger: messenger
+  end
+
+  defp put_headers(conn, key_values) do
+    Enum.reduce key_values, conn, fn {k, v}, conn ->
+      Plug.Conn.put_resp_header(conn, to_string(k), v)
+    end
   end
 
 end
