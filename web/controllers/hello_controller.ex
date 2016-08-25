602a956f35211339510d9344e20806ee1fcd5307
byelipk <byelipk@gmail.com>
2016-08-25 12:06:35 -0400

    initial

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,13 @@
+defmodule HelloPhoenix.HelloController do
+
+  use HelloPhoenix.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html"
+  end
+
+end
